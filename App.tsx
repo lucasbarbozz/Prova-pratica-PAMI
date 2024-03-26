f0614417e9e1a74305068153e9913a95462e55ab
lucasbarbozz <lucasbarboza4235@gmail.com>
2024-03-25 21:00:02 -0300

    ajuste

--- App.tsx
+++ App.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgb(18, 18, 18)',
     justifyContent: 'center',
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   
   letra: {
